cc072d9bdf09cd176cc9e4bf4552f16b7fffb571
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-09-03 22:44:45 +0530

    handlebars

--- expressgenerator/routes/index.js
+++ expressgenerator/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const values=["abin","ameer","shah","hoi"]
+  const person={name:"abin",comments:{comment:"kollam pwoli",date:"07-08-2020"}}
+  const per={name:"abinsh",admin:false}
+  res.render('index', { title: 'Express',values,person ,per});
 });
 
 module.exports = router;
